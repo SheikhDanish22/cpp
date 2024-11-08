fbbbc8b2ef2b05b4345bb70f2bb0b2fa9a6e03c9
SHEIKH DANISH <danish1521@gmail.com>
2024-11-08 16:27:14 +0530

    add

---
+++ addition.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a,b,c,d,e,f;
+	cout<<"Enter first value and Enter secound value=";
+	cin>>a>>b;
+	c=a+b;
+	d=a*b;
+	e=a-b;
+	f=a/b;
+	
+	
+	cout<<"c="<<c;
+	
+	;
+}
