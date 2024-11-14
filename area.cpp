a32d52d740d6c652233cdf26f035d1755ee9a237
SHEIKH DANISH <danish1521@gmail.com>
2024-11-14 11:19:17 +0530

    area

---
+++ area.cpp
@@ -0,0 +1,48 @@
+/*#include<iostream>
+using namespace std;
+class Circle{
+	public:
+	double circle_area;
+	double circle_para;
+	double radius;
+	
+	Circle(){
+//	 circle_area=0;
+//		circle_para=0;
+		radius=0;
+		cout<<"constructor\n";
+		
+	}
+	Circle(double r){	
+		radius=r;
+			cout<<"parametrized calling\n";
+			
+	}
+	
+	void insert_data(){
+		cout<<"enter your redius of circle:";
+		cin>>radius;
+	}
+		
+	void display_data(){
+	 double circle_area=(3.14)*radius*radius;
+	 cout<<"circle of area:"<<circle_area<<endl;
+	 
+	  double circle_para=2*3.14*radius;
+	 cout<<"circle of para:"<<circle_para<<endl;
+		
+	}
+};
+
+int main(){
+     Circle sys1;
+     sys1.insert_data();   
+     sys1.display_data();
+       Circle sys2(5);
+     sys2.display_data();
+}*/
+
+
+
+
+
